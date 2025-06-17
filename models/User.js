20088fbd30253ca97864b869abdb9577ad0ce42b
ahbisek <abhisek1999rai@gmail.com>
2025-06-17 22:19:01 +0545

    updates

--- models/User.js
+++ models/User.js
@@ -36,22 +36,4 @@ module.exports = mongoose.model(
     "User", UserSchema
 )
 
-Userroutes//
-
-const express = require("express")
-const router = express.Router()
-
-const { registerUser, loginUser } = require("../controllers/userController") 
-
-router.post(
-    "/register",
-    registerUser
-)
-
-router.post(
-    "/login",
-    loginUser
-)
-module.exports = router
-
 
