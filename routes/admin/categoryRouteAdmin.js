b55bc151f084834a3b8140b536964584efca22ec
ahbisek <abhisek1999rai@gmail.com>
2025-06-21 22:49:22 +0545

    Route for category admin

--- routes/admin/categoryRouteAdmin.js
+++ routes/admin/categoryRouteAdmin.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const router = express.Router();
-// const {createCategory} = require('../../controllers/admin/categorymanagement');
-const categoryController = require('../../controllers/admin/categorymanagement');
-// can be implemented using single import
-const upload = require("../../middlewares/fileupload")
+// const express = require('express');
+// const router = express.Router();
+// // const {createCategory} = require('../../controllers/admin/categorymanagement');
+// const categoryController = require('../../controllers/admin/categorymanagement');
+// // can be implemented using single import
+// const upload = require("../../middlewares/fileupload")
 
 
-router.post(
-    '/',
-    upload.single("image"),
-    categoryController.createCategory
-);
-router.get('/', categoryController.getAllCategories);
-router.get('/:id', categoryController.getCategoryById);
-router.put('/:id',
-    upload.single("image"),
-    categoryController.updateCategory);
-router.delete('/:id', categoryController.deleteCategory);
+// router.post(
+//     '/',
+//     upload.single("image"),
+//     categoryController.createCategory
+// );
+// router.get('/', categoryController.getAllCategories);
+// router.get('/:id', categoryController.getCategoryById);
+// router.put('/:id',
+//     upload.single("image"),
+//     categoryController.updateCategory);
+// router.delete('/:id', categoryController.deleteCategory);
 
-module.exports = router;
+// module.exports = router;
