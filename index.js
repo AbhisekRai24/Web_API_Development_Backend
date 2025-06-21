a3324f708e75f395ad0df9428a8f48befe30b54a
ahbisek <abhisek1999rai@gmail.com>
2025-06-21 11:50:57 +0545

    Made new Api

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoute"); // assuming login/register inside this
-const cors = require("cors")
+const userRoutes = require("./routes/userRoute");
+const adminCategoryRoutes = require("./routes/admin/categoryRouteAdmin")
+const adminProductRoutes = require("./routes/admin/productRouteAdmin")
 
+const cors = require("cors")
+const path = require("path")
 const app = express();
 
 app.use(express.json());
 let corsOptions = {
-  origin: "*" // or list of domain to whitelist
+  origin: "*"
 }
 app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 // Connect to DB
 connectDB();
 
 // login and register route is in here
 app.use("/api/auth", userRoutes);
+app.use("/api/admin/category", adminCategoryRoutes)
+app.use("/api/admin/product", adminProductRoutes)
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ routes/admin/productRouteAdmin.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+const productController = require("../../controllers/admin/productmanagement")
+
+
+router.post(
+    "/",
+    productController.createProduct
+)
+router.get(
+    "/",
+    productController.getProducts
+)
+module.exports = router
